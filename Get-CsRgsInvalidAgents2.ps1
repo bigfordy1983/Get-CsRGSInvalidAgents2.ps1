e0cc12fb873cdbb4f3e11a6af40748e5f5e95cb4
bigfordy1983 <dan.jr.ford@gmail.com>
2017-06-17 20:22:15 +0100

    Update Repository

---
+++ Get-CsRgsInvalidAgents2.ps1
@@ -0,0 +1,49 @@
+﻿<#
+.DESCRIPTION
+	This script check and remove invalid Response Group agents from Lync Server 2010 and 2013.
+
+.NOTES
+  Version      	   		: 1.1
+  Author    			: David Paulino http://blogs.technet.com/b/uclobby/, Dan Ford
+  
+
+#>
+
+
+[CmdletBinding()]
+param(
+[parameter(ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true)]
+	[switch] $Remove,
+[parameter(ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true)]
+	[string] $Name
+)
+
+$startTime=Get-Date;
+$users = get-csuser | Select-object DisplayName, Sipaddress, enabled
+
+
+if(!$Name){
+    $agentGroups = Get-CsRgsAgentGroup | Sort-Object Name
+} else {
+    $agentGroups = Get-CsRgsAgentGroup -Name $Name | Sort-Object Name   
+}
+
+
+foreach($agentGroup in $agentGroups){
+    $agents = $agentGroup | Select-Object -ExpandProperty AgentsByURI
+    Write-Host "Checking Invalid users in" $agentGroup.Name -ForegroundColor Cyan
+    foreach($agent in $agents){
+        if(($users | ?{($_.SipAddress -eq $agent.AbsoluteUri -and $_.Enabled)} | Measure).count -eq 0){
+            if($remove){
+                Write-Host "Removing invalid user:" $agent.AbsoluteUri -ForegroundColor Green
+                $agentGroup.AgentsByUri.Remove($agent.AbsoluteUri)
+                Set-CsRgsAgentGroup -Instance $agentGroup
+            }else{
+                Write-Host "Invalid user:" $agent.AbsoluteUri -ForegroundColor Yellow}
+            }
+        }
+    }
+    
+$endTime = Get-Date
+$totalTime= [math]::round(($endTime - $startTime).TotalSeconds,2)
+Write-Host "Execution time:" $totalTime "seconds" -ForegroundColor Cyan
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Get-CsRGSInvalidAgents2.ps1
+
+This is a Skype for Business Powershell script designed to clean up "Ghost" users who have been disabled but still appear in Response Groups.
+
+This Script is a modification of the rather excellent original script by David Paulino http://blogs.technet.com/b/uclobby/
+It improves the runtime by storing a get-csuser to a variable and calling the variable in the for-each loop, rather than running cs-user on each loop.
+This script runs itself, it has two Parameters, -Name where you can specify the name of a specific Response Group and -Remove which automatically removes the user from the response group
+
+To run, just run the script.
